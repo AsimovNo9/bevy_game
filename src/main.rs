17752e87ceb339ea619dbbb1799993a2f482dc76
AsimovNo9 <adedamola.sode@gmail.com>
2025-11-28 15:35:57 +0000

    Added files

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Player;
+fn main(){
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup)
+    .run();
+}
+
+fn setup(mut commands: Commands){
+    commands.spawn(Camera2d);
+
+
+    // Code Update Alert
+    commands.spawn((
+        Text2d::new("@"),
+        TextFont{
+            font_size: 12.0,
+            font: default(),
+            ..default()
+        },
+        TextColor(Color::WHITE),
+        Transform::from_translation(Vec3::ZERO),
+        Player,
+    ));
+}
